0b6fc0667b87cba1d9b9db95707eaaa3a51c5dab
Sofya Bochkaryova <sofya.bochkaryova@gmail.com>
2026-05-21 14:48:49 +0300

    Fix database config for Railway

--- bit_platform/bit_platform/settings.py
+++ bit_platform/bit_platform/settings.py
@@ -89,18 +89,10 @@ WSGI_APPLICATION = 'bit_platform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "db_bit",
-        "USER": "postgres",
-        "PASSWORD": "Test1234",
-        "HOST": "localhost",
-        "PORT": "5432",
-        "OPTIONS": {
-            "client_encoding": "UTF8",
-        },
-    }
+    'default': dj_database_url.config(default='postgresql://postgres:Test1234@localhost:5432/db_bit')
 }
 
 
