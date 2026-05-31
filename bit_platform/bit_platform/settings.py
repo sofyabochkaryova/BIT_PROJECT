9a5c6b7850c369f9839bf17605ee445640921954
Sofya Bochkaryova <sofya.bochkaryova@gmail.com>
2026-05-31 18:02:30 +0300

    изменение шаблона2

--- bit_platform/bit_platform/settings.py
+++ bit_platform/bit_platform/settings.py
@@ -91,11 +91,10 @@ WSGI_APPLICATION = 'bit_platform.wsgi.application'
 
 import dj_database_url
 
+
 DATABASES = {
     'default': dj_database_url.config()
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
